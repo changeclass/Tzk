363a26aa1280a9db174ddc884b39796c17371471
Dreamy-TZK <dreamytzk@outlook.com>
2020-11-01 15:30:54 +0800

    :sparkles: 添加新的学习代码

---
+++ 2020-11/01/Stable/index.js
@@ -0,0 +1,69 @@
+let app = new Vue({
+  el: ".main",
+  data: {
+    todoItem: [],
+    todoContent: "",
+    // 0为全部 1为未完成 2为已完成
+    state: "0",
+  },
+  computed: {
+    showTodos: {
+      get() {
+        let lists = [];
+        this.todoItem.forEach((e) => {
+          if (this.state == "0") {
+            if (e.show) {
+              lists.push(e);
+            }
+          } else if (this.state == "1") {
+            if (e.show && !e.state) {
+              lists.push(e);
+            }
+          } else {
+            if (e.show && e.state === true) {
+              lists.push(e);
+            }
+          }
+        });
+        return lists;
+      },
+    },
+  },
+  methods: {
+    // 添加待办
+    add() {
+      let item = {
+        //  待办事件的ID 唯一
+        id: this.todoItem.length,
+        // 待办事件内容
+        content: this.todoContent,
+        // 待办时间的状态
+        state: false,
+        // 是否删除
+        show: true,
+      };
+      this.todoItem.push(item);
+      this.todoContent = "";
+    },
+    // 删除
+    destroy(id) {
+      this.todoItem.forEach((element, index) => {
+        if (element.id == id) {
+          this.todoItem[id].show = false;
+        }
+      });
+    },
+    //TODO 改变内容
+    chamgeContent(id, content) {
+      this.todoItem.forEach((element, index) => {
+        if (element.id == id) {
+          this.todoItem[id].content = false;
+        }
+      });
+    },
+    // 已完成的事件
+    screen(flag) {
+      this.state = flag;
+    },
+  },
+});
