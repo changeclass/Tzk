16b01c35dfdd9155eb2d1f639cf3eda47d24bfe9
XiaoKang <dreamytzk@outlook.com>
2020-11-22 16:31:57 +0800

    ✨ 修改api

--- 2020-11/react-admin-server/server.js
+++ 2020-11/react-admin-server/server.js
@@ -12,21 +12,21 @@ const app = express() // 产生应用对象
 // 声明使用静态中间件
 app.use(express.static('public'))
 // 声明使用解析post请求的中间件
-app.use(express.urlencoded({extended: true})) // 请求体参数是: name=tom&pwd=123
+app.use(express.urlencoded({ extended: true })) // 请求体参数是: name=tom&pwd=123
 app.use(express.json()) // 请求体参数是json结构: {name: tom, pwd: 123}
 // 声明使用解析cookie数据的中间件
 const cookieParser = require('cookie-parser')
 app.use(cookieParser())
 // 声明使用路由器中间件
 const indexRouter = require('./routers')
-app.use('/', indexRouter)  //
+app.use('/api', indexRouter)  //
 
 const fs = require('fs')
 
 // 必须在路由器中间之后声明使用
-/*app.use((req, res) => {
-  fs.readFile(__dirname + '/public/index.html', (err, data)=>{
-    if(err){
+app.use((req, res) => {
+  fs.readFile(__dirname + '/public/index.html', (err, data) => {
+    if (err) {
       console.log(err)
       res.send('后台错误')
     } else {
@@ -36,10 +36,10 @@ const fs = require('fs')
       res.end(data)
     }
   })
-})*/
+})
 
 // 通过mongoose连接数据库
-mongoose.connect('mongodb://localhost/server_db2', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost/server_db2', { useNewUrlParser: true })
   .then(() => {
     console.log('连接数据库成功!!!')
     // 只有当连接上数据库后才去启动服务器
