341e240f77ce7cc5009aeaab8e2afc7f87b12421
XiaoKang <dreamytzk@outlook.com>
2020-11-23 16:47:55 +0800

    ✨ 文件上传

--- 2020-11/zhihu-api/app/controllers/home.js
+++ 2020-11/zhihu-api/app/controllers/home.js
@@ -5,6 +5,7 @@ class HomeCtl {
   }
   upload (ctx) {
     const file = ctx.request.files.file
+    console.log(ctx.request.files);
     const baseName = path.basename(file.path)
     ctx.body = { url: `${ctx.origin}/uploads/${baseName}` }
   }
