036a71e5e4d79e579dbea5b758348e35b3bcf01f
XiaoKang <dreamytzk@outlook.com>
2020-11-23 15:58:51 +0800

    ✨ 上传文件接口

--- 2020-11/zhihu-api/app/index.js
+++ 2020-11/zhihu-api/app/index.js
@@ -1,9 +1,10 @@
 const Koa = require('koa')
-const bodyparser = require('koa-bodyparser')
+const koaBody = require('koa-body')
 const error = require('koa-json-error')
 const parameter = require('koa-parameter')
 const mongoose = require('mongoose')
-
+const path = require('path')
+const koaStatic = require('koa-static')
 const app = new Koa()
 const routing = require('./routes/index')
 // 通过配置文件导入链接字符串
@@ -11,6 +12,8 @@ const { connectionStr } = require('./config')
 mongoose.connect(connectionStr, { useUnifiedTopology: true, useNewUrlParser: true }, () => {
   console.log('链接成功了');
 })
+// 静态文件
+app.use(koaStatic(path.join(__dirname, 'public')))
 
 // 默认配置
 app.use(error({
@@ -18,7 +21,17 @@ app.use(error({
 }))
 
 
-app.use(bodyparser())
+app.use(koaBody({
+  // 启用文件传输
+  multipart: true,
+  formidable: {
+    // 上传目录
+    uploadDir: path.join(__dirname, '/public/uploads'),
+    // 保留扩展名
+    keepExtensions: true
+  }
+}))
+
 app.use(parameter(app))
 routing(app)
 app.listen(3000, () => {
