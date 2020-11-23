e28e4b2760fc105c74a7bd00da00e39cbc2a3f43
XiaoKang <dreamytzk@outlook.com>
2020-11-23 15:10:33 +0800

    ✨ 用户认证

--- 2020-11/zhihu-api/app/routes/users.js
+++ 2020-11/zhihu-api/app/routes/users.js
@@ -1,15 +1,27 @@
 const Router = require('koa-router')
+const jsonwebtoken = require('jsonwebtoken')
 
 const router = new Router({ prefix: '/users' })
-
 const { find, findById, delete: del, update, create, login } = require('../controllers/user')
+const { secret } = require('../config')
 
+const auth = async (ctx, next) => {
+  const { authorization = '' } = ctx.request.header
+  const token = authorization.replace("Bearer ", '')
+  try {
+    const user = jsonwebtoken.verify(token, secret)
+    ctx.state.user = user
+  } catch (err) {
+    ctx.throw(401, err.message)
+  }
+  await next()
+}
 
 router.get('/', find)
 router.post('/', create)
 router.get('/:id', findById)
-router.patch('/:id', update)
-router.delete('/:id', del)
+router.patch('/:id', auth, update)
+router.delete('/:id', auth, del)
 // 用户登录
 router.post('/login', login)
 
