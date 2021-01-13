190b78cb7e309625b235464854f90f5d18ef8e13
XiaoKang <dreamytzk@outlook.com>
2021-01-13 18:31:13 +0800

    ✨ 无缝滚动

--- 2021-01/13/03. 轮播图/src/js/index.js
+++ 2021-01/13/03. 轮播图/src/js/index.js
@@ -7,27 +7,44 @@ const wrapper = container.querySelector('.swiper-wrapper')
 // 获取所有图片
 const slides = container.querySelectorAll('.swiper-slide')
 // 当前显示的图片的下标
-let index = 0
+var index = 0
 // 获取所有小圆点
 let dots = null
 // 获取小圆点的容器
 const pagination = container.querySelector('.swiper-pagination')
+
+// 无缝滚动
+wrapper.innerHTML += wrapper.innerHTML
+const slidesNew = container.querySelectorAll('.swiper-slide')
 app.addEventListener('touchstart', function (e) {
   e.preventDefault()
 })
 
 // 触摸开始事件
 container.addEventListener('touchstart', function (e) {
+  // 移除过度效果
+  wrapper.style.transition = 'none'
+  // 获取按下时的时间点
+  this.touchStartTime = Date.now()
+  // 判断索引下标
+  if (index == 0) {
+    index = slides.length
+    const newLeft = -index * container.offsetWidth
+    // 设置left的样式
+    wrapper.style.left = newLeft + 'px'
+  }
+  // 右侧边界
+  if (index == slidesNew.length - 1) {
+    index = slides.length - 1
+    const newLeft = -index * container.offsetWidth
+    // 设置left的样式
+    wrapper.style.left = newLeft + 'px'
+  }
   // 获取触摸开始时的触点位置
   this.x = e.touches[0].clientX
 
   // 获取包裹元素的水平偏移量
   this.left = wrapper.offsetLeft
-
-  // 移除过度效果
-  wrapper.style.transition = 'none'
-  // 获取按下时的时间点
-  this.touchStartTime = Date.now()
 })
 
 // 触摸滑动时事件
@@ -68,8 +85,8 @@ container.addEventListener('touchend', function (e) {
   if (index < 0) {
     index = 0
   }
-  if (index > slides.length - 1) {
-    index = slides.length - 1
+  if (index > slidesNew.length - 1) {
+    index = slidesNew.length - 1
   }
 
   /**
@@ -88,7 +105,7 @@ container.addEventListener('touchend', function (e) {
     dot.classList.remove('active')
   })
   // 当前索引的导航点 增加 active 类
-  dots[index].classList.add('active')
+  dots[index % slides.length].classList.add('active')
 })
 
 // 初始化函数
@@ -96,14 +113,14 @@ function init() {
   // 容器设置 高度
   window.onload = function () {
     // 获取第一个图片的高度
-    const h = slides[0].offsetHeight
+    const h = slidesNew[0].offsetHeight
     container.style.height = h + 'px'
   }
   // 包裹元素的宽度
-  wrapper.style.width = slides.length * 100 + '%'
+  wrapper.style.width = slidesNew.length * 100 + '%'
   // 设置子元素的宽度
-  slides.forEach((item) => {
-    item.style.width = 100 / slides.length + '%'
+  slidesNew.forEach((item) => {
+    item.style.width = 100 / slidesNew.length + '%'
   })
   // 根据图片数量动态创建导航点的个数
   for (var i = 0; i < slides.length; i++) {
