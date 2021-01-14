859d3baa719c1991a2fb55819e22514fcba6f6ff
XiaoKang <dreamytzk@outlook.com>
2021-01-14 19:33:57 +0800

    ✨ 导航栏可拖拽

--- 2021-01/13/05. 音乐台/src/js/index.js
+++ 2021-01/13/05. 音乐台/src/js/index.js
@@ -18,3 +18,38 @@ import './swiper'
       document.documentElement.clientWidth / 10 + 'px'
   }
 })()
+
+// 头部逻辑
+;(function () {
+  const menuBtn = document.querySelector('#header .up .menu')
+  const zhezhao = document.querySelector('#header .zhezhao')
+  const app = document.querySelector('#app')
+  menuBtn.addEventListener('touchstart', function () {
+    // 使按钮的状态改变
+    this.classList.toggle('open')
+    // 使菜单的显示状态改变
+    zhezhao.classList.toggle('open')
+  })
+  const input = document.querySelector('#search')
+  input.addEventListener('touchstart', function (e) {
+    e.stopPropagation()
+  })
+  app.addEventListener('touchstart', function (e) {
+    input.blur()
+  })
+})()
+;(function () {
+  var nav = document.querySelector('#nav')
+  var wrap = nav.querySelector('.wrap')
+  nav.addEventListener('touchstart', function (e) {
+    // 获取起始点触点位置
+    this.x = e.touches[0].clientX
+    // 获取当前包裹元素的偏移量
+    this.left = transformCSS(wrap, 'translateX')
+  })
+  nav.addEventListener('touchmove', function (e) {
+    this._x = e.touches[0].clientX
+    const newLeft = this._x - this.x + this.left
+    transformCSS(wrap, 'translateX', newLeft)
+  })
+})()
