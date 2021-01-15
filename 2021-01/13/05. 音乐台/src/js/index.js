e891e70bfab85218bd9a49810bf4f688adce7fa8
XiaoKang <dreamytzk@outlook.com>
2021-01-15 09:37:37 +0800

    ✨ 过渡回弹

--- 2021-01/13/05. 音乐台/src/js/index.js
+++ 2021-01/13/05. 音乐台/src/js/index.js
@@ -46,10 +46,46 @@ import './swiper'
     this.x = e.touches[0].clientX
     // 获取当前包裹元素的偏移量
     this.left = transformCSS(wrap, 'translateX')
+    wrap.style.transition = 'none'
+    // 获取触摸开始的时间
+    this.startTime = Date.now()
   })
   nav.addEventListener('touchmove', function (e) {
     this._x = e.touches[0].clientX
     const newLeft = this._x - this.x + this.left
     transformCSS(wrap, 'translateX', newLeft)
+  })
+  // 绑定触摸结束事件
+  nav.addEventListener('touchend', function (e) {
+    // 获取当前包裹元素的translateX的值
+    let translateX = transformCSS(wrap, 'translateX')
+    // 获取触摸的位移
+    this._x = e.changedTouches[0].clientX
+    // 计算位置
+    const disX = this._x - this.x
+    // 获取触摸结束的时间
+    this.endTime = Date.now()
+    const disTime = this.endTime - this.startTime
+    // 计算速度
+    const v = disX / disTime
+    // 位移
+    const s = v * 100
+    translateX += s
+    // 过渡切换
+    wrap.style.transition = '.5s ease-out'
+    transformCSS(wrap, 'translateX', translateX)
+    console.log(disX, disTime)
+    // 判断是否越界
+    if (translateX > 0) {
+      // 增加过渡
+      wrap.style.transition = '.5s cubic-bezier(.21,.68,.42,1.77)'
+      transformCSS(wrap, 'translateX', 0)
+    }
+    const minTranslateX = nav.offsetWidth - wrap.offsetWidth
+    if (translateX < minTranslateX) {
+      // 增加过渡
+      wrap.style.transition = '.5s cubic-bezier(.21,.68,.42,1.77)'
+      transformCSS(wrap, 'translateX', minTranslateX)
+    }
   })
 })()
