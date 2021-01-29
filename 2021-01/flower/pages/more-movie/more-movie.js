d7837e9af9d3995bb932fc085cc1ea3195667437
XiaoKang <dreamytzk@outlook.com>
2021-01-29 15:39:40 +0800

    ✨ 电影业跳转详情页 ✨ 搜索栏

--- 2021-01/flower/pages/more-movie/more-movie.js
+++ 2021-01/flower/pages/more-movie/more-movie.js
@@ -1,65 +1,32 @@
-// pages/movies/movies.js
+// pages/more-movie/more-movie.js
 const app = getApp()
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    inTheaters: [],
-    comingSoon: [],
-    top250: [],
-    searchResult: false,
-    searchData: []
+    movies: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: async function (options) {
+  onLoad: function (options) {
+    const type = options.type
     wx.request({
-      url: app.gBaseUrl + 'in_theaters',
+      url: app.gBaseUrl + type,
       data: {
         start: 0,
-        count: 3
+        count: 12
       },
       success: (res) => {
         this.setData({
-          inTheaters: res.data.subjects
+          movies: res.data.subjects
         })
       }
     })
+  },
 
-    wx.request({
-      url: app.gBaseUrl + 'coming_soon',
-      data: {
-        start: 0,
-        count: 3
-      },
-      success: (res) => {
-        this.setData({
-          comingSoon: res.data.subjects
-        })
-      }
-    })
-    wx.request({
-      url: app.gBaseUrl + 'top250',
-      data: {
-        start: 0,
-        count: 3
-      },
-      success: (res) => {
-        this.setData({
-          top250: res.data.subjects
-        })
-      }
-    })
-  },
-  onGoToMore(event) {
-    const type = event.currentTarget.dataset.type
-    wx.navigateTo({
-      url: '/pages/more-movie/more-movie?type=' + type
-    })
-  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
