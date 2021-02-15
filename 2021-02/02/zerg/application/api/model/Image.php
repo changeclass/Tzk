31b48ae5a5c71d5860fb01ec878861c2b272c78e
XiaoKang <dreamytzk@outlook.com>
2021-02-15 16:28:19 +0800

    feature 共同基类封装

--- 2021-02/02/zerg/application/api/model/Image.php
+++ 2021-02/02/zerg/application/api/model/Image.php
@@ -4,16 +4,10 @@
 namespace app\api\model;
 
 
-use think\Model;
-
-class Image extends Model
+class Image extends BaseModel
 {
     protected $hidden = ['update_time','delete_time','from','id'];
     public function getUrlAttr($value,$data){
-        $finalUrl = $value;
-        if($data['from'] == 1){
-            $finalUrl = config('setting.img_prefix').$value;
-        }
-        return $finalUrl;
+        return $this->prefixImgUrl($value,$data);
     }
 }
