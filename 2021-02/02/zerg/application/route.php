b86786d1aeda83754d33f95a275b5434520a8d96
XiaoKang <dreamytzk@outlook.com>
2021-02-16 20:19:22 +0800

    feat：路由分组

--- 2021-02/02/zerg/application/route.php
+++ 2021-02/02/zerg/application/route.php
@@ -17,13 +17,15 @@ Route::get('api/:version/theme','api/:version.Theme/getSimpleList');
 /* 获取单个主题 */
 Route::get('api/:version/theme/:id','api/:version.Theme/getComplexOne');
 
-/* 获取最近商品 */
-Route::get('api/:version/product/recent','api/:version.Product/getRecent');
-/* 获取商品分类 */
-Route::get('api/:version/product/by_category','api/:version.Product/getAllInCategory');
-/* 获取某个商品 */
-Route::get('api/:version/product/:id','api/:version.Product/getOne');
-
+/* 产品相关路由 */
+Route::group('api/:version/product',function (){
+    /* 获取最近商品 */
+    Route::get('/recent','api/:version.Product/getRecent');
+    /* 获取商品分类 */
+    Route::get('/by_category','api/:version.Product/getAllInCategory');
+    /* 获取某个商品 */
+    Route::get('/:id','api/:version.Product/getOne',[],['id'=>'\d+']);
+});
 
 /* 获取分类 */
 Route::get('api/:version/category/all','api/:version.Category/getAllCategories');
