77c6122fd6cd6757f207502cc4544b7a6b2c1703
XiaoKang <dreamytzk@outlook.com>
2021-02-16 11:54:47 +0800

    feat：路由完整匹配 feat：完成theme接口

--- 2021-02/02/zerg/application/api/controller/v1/Theme.php
+++ 2021-02/02/zerg/application/api/controller/v1/Theme.php
@@ -6,6 +6,7 @@ namespace app\api\controller\v1;
 
 use app\api\validate\IDCollection;
 use app\api\model\Theme as ThemeModel;
+use app\api\validate\IDMustBePostiveInt;
 use app\lib\exception\ThemeException;
 
 class Theme
@@ -23,5 +24,16 @@ class Theme
             throw new ThemeException();
         }
         return $result;
+    }
+    /*
+     * @url /theme/:id
+     * */
+    public function getComplexOne($id){
+        ((new IDMustBePostiveInt())->goCheck());
+        $theme = ThemeModel::getThemeWithProducts($id);
+        if(!$theme){
+            throw new ThemeException();
+        }
+        return $theme;
     }
 }
