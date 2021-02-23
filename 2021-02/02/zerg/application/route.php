d9363c28f9e44d518993bd074d940134042000a6
XiaoKang <dreamytzk@outlook.com>
2021-02-23 17:37:48 +0800

    feat:订单详情

--- 2021-02/02/zerg/application/route.php
+++ 2021-02/02/zerg/application/route.php
@@ -39,6 +39,10 @@ Route::post('api/:version/address','api/:version.Address/createOrUpdateAddress')
 /* 订单相关 */
 Route::post('api/:version/order','api/:version.Order/placeOrder');
 Route::get('api/:version/order/by_user', 'api/:version.Order/getSummaryByUser');
+Route::get('api/:version/order/:id', 'api/:version.Order/getDetail',
+    [],['id' => '\d+']);
+Route::get('api/:version/order/paginate', 'api/:version.Order/getSummary');
+Route::put('api/:version/order/delivery', 'api/:version.Order/delivery');
 
 /* 支付相关 */
 Route::post('api/:version/pay/per_order','api/:version.Pay/getPerOrder');
